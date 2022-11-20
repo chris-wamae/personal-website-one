1a2745975f243e819e2f0aec221ebad294e631c4
chris-wamae <chriswamae123@gmail.com>
2022-11-20 21:26:32 +0300

    created, linked and added events to index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const nav = document.getElementById("nav-1")
+
+nav.addEventListener("mouseover",function(){
+nav.style.backgroundColor = "red"})
+
+nav.addEventListener("mouseout",function()
+{nav.style.backgroundColor = "#F3C178"})
+
+const nav2 = document.getElementById("nav-2")
+
+nav2.addEventListener("mouseover",function(){
+nav2.style.backgroundColor = "red"})
+
+nav2.addEventListener("mouseout",function()
+{nav2.style.backgroundColor = "#F3C178"})
+
+const nav3 = document.getElementById("nav-3")
+
+nav3.addEventListener("mouseover",function(){
+nav3.style.backgroundColor = "red"})
+
+nav3.addEventListener("mouseout",function()
+{nav3.style.backgroundColor = "#F3C178"})
